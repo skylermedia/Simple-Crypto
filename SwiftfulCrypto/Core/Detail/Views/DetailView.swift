3a6b3211ed221bb0354cfd9f584d01cf2b191a6c
Skyler Media Co <github@healthyteen.org>
2023-12-18 11:09:51 -0800

    Initial Commit

---
+++ SwiftfulCrypto/Core/Detail/Views/DetailView.swift
@@ -0,0 +1,195 @@
+//
+//  DetailView.swift
+//  SwiftfulCrypto
+//
+//  Created by Nick Sarno on 5/11/21.
+//
+
+import SwiftUI
+
+struct DetailLoadingView: View {
+    
+    @Binding var coin: CoinModel?
+
+    var body: some View {
+        ZStack {
+            if let coin = coin {
+                DetailView(coin: coin)
+            }
+        }
+    }
+    
+}
+
+struct DetailView: View {
+    
+    @StateObject private var vm: DetailViewModel
+    @State private var showFullDescription: Bool = false
+    private let columns: [GridItem] = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+    ]
+    private let spacing: CGFloat = 30
+    
+    init(coin: CoinModel) {
+        _vm = StateObject(wrappedValue: DetailViewModel(coin: coin))
+    }
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                VStack(spacing: 20) {
+                    chartTitle
+                    ChartView(coin: vm.coin)
+                        .padding(.vertical)
+                    overviewTitle
+                    Divider()
+                    descriptionSection
+                    overviewGrid
+                    additionalTitle
+                    Divider()
+                    additionalGrid
+                    linksTitle
+                    Divider()
+                    linksSection
+                }
+                .padding()
+            }
+        }
+        .background(
+            Color.theme.background
+                .ignoresSafeArea()
+        )
+        .navigationTitle(vm.coin.name)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) {
+                navigationBarTrailingItems
+            }
+        }
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            DetailView(coin: dev.coin)
+        }
+    }
+}
+
+extension DetailView {
+    
+    private var navigationBarTrailingItems: some View {
+        HStack {
+            Text(vm.coin.symbol.uppercased())
+                .font(.headline)
+                .foregroundColor(Color.theme.secondaryText)
+            CoinImageView(coin: vm.coin)
+                .frame(width: 25, height: 25)
+        }
+    }
+    
+    private var chartTitle: some View {
+        Text("1 Week Price")
+            .font(.title)
+            .bold()
+            .foregroundColor(Color.theme.accent)
+            .frame(maxWidth: .infinity, alignment: .leading)
+    }
+    
+    private var overviewTitle: some View {
+        Text("Overview")
+            .font(.title)
+            .bold()
+            .foregroundColor(Color.theme.accent)
+            .frame(maxWidth: .infinity, alignment: .leading)
+    }
+    
+    private var additionalTitle: some View {
+        Text("Additional Details")
+            .font(.title)
+            .bold()
+            .foregroundColor(Color.theme.accent)
+            .frame(maxWidth: .infinity, alignment: .leading)
+    }
+    
+    private var linksTitle: some View {
+        Text("Links")
+            .font(.title)
+            .bold()
+            .foregroundColor(Color.theme.accent)
+            .frame(maxWidth: .infinity, alignment: .leading)
+    }
+    
+    private var descriptionSection: some View {
+        ZStack {
+            if let coinDescription = vm.coinDescription,
+               !coinDescription.isEmpty {
+                VStack(alignment: .leading) {
+                    Text(coinDescription)
+                        .lineLimit(showFullDescription ? nil : 3)
+                        .font(.callout)
+                        .foregroundColor(Color.theme.secondaryText)
+
+                    Button(action: {
+                        withAnimation(.easeInOut) {
+                            showFullDescription.toggle()
+                        }
+                    }, label: {
+                        Text(showFullDescription ? "Collapse" : "Read More...")
+                            .font(.caption)
+                            .fontWeight(.bold)
+                            .padding(.vertical, 4)
+                    })
+                    .accentColor(.blue)
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+            }
+        }
+    }
+    
+    private var overviewGrid: some View {
+        LazyVGrid(
+            columns: columns,
+            alignment: .leading,
+            spacing: spacing,
+            pinnedViews: [],
+            content: {
+                ForEach(vm.overviewStatistics) { stat in
+                    StatisticView(stat: stat)
+                }
+        })
+    }
+    
+    private var additionalGrid: some View {
+        LazyVGrid(
+            columns: columns,
+            alignment: .leading,
+            spacing: spacing,
+            pinnedViews: [],
+            content: {
+                ForEach(vm.additionalStatistics) { stat in
+                    StatisticView(stat: stat)
+                }
+        })
+    }
+    
+    private var linksSection: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            if let websiteString = vm.websiteURL,
+               let url = URL(string: websiteString) {
+                Link("Website", destination: url)
+            }
+            
+            if let redditString = vm.redditURL,
+               let url = URL(string: redditString) {
+                Link("Reddit", destination: url)
+            }
+            
+        }
+        .accentColor(.blue)
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .font(.headline)
+    }
+    
+}
---
+++ SwiftfulCrypto/Core/Settings/Views/SettingsView.swift
@@ -0,0 +1,102 @@
+//
+//  SettingsView.swift
+//  SwiftfulCrypto
+//
+//  Created by Nick Sarno on 5/14/21.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    
+    let portfolioURL = URL(string: "https://www.skyler.media")!
+    let placeholderURL = URL(string: "https://www.google.com")!
+    let coingeckoURL = URL(string: "https://www.coingecko.com")!
+    
+    var body: some View {
+        NavigationView {
+            ZStack {
+                // background
+                Color.theme.background
+                    .ignoresSafeArea()
+                
+                // content
+                List {
+                    portfolioSection
+                        .listRowBackground(Color.theme.background.opacity(0.5))
+                    coinGeckoSection
+                        .listRowBackground(Color.theme.background.opacity(0.5))
+                    applicationSection
+                        .listRowBackground(Color.theme.background.opacity(0.5))
+                }
+            }
+            .font(.headline)
+            .accentColor(.blue)
+            .listStyle(GroupedListStyle())
+            .navigationTitle("Information")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    XMarkButton()
+                }
+            }
+        }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
+
+extension SettingsView {
+    
+    private var portfolioSection: some View {
+        Section(header: Text("Development & Portfolio")) {
+            VStack(alignment: .leading) {
+                Image("logo")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                Text("This app was made by Skyler with a lot of hard work!")
+                    .font(.callout)
+                    .fontWeight(.medium)
+                    .foregroundColor(Color.theme.accent)
+                Divider()
+                Spacer()
+                Text("©2023 Skyler Media Co.")
+                    .font(.callout)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.theme.accent)
+            }
+            .padding(.vertical)
+            Link("Visit My Portfolio!", destination: portfolioURL)
+        }
+    }
+    
+    private var coinGeckoSection: some View {
+        Section(header: Text("CoinGecko")) {
+            VStack(alignment: .leading) {
+                Image("coingecko")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 100)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                Text("The cryptocurrency data used in this app comes from a free API developed by CoinGecko! Prices may be slightly delayed.")
+                    .font(.callout)
+                    .fontWeight(.medium)
+                    .foregroundColor(Color.theme.accent)
+            }
+            .padding(.vertical)
+            Link("Visit CoinGecko 🦎", destination: coingeckoURL)
+        }
+    }
+    
+    private var applicationSection: some View {
+        Section(header: Text("Application")) {
+            Link("Terms of Service", destination: placeholderURL)
+            Link("Privacy Policy", destination: placeholderURL)
+        }
+    }
+    
+    
+}
